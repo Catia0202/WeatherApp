63b8b8968d83a4a557b60575da3a53cfe63d4bc8
catia <catia@MSI>
2022-12-20 19:06:11 +0000

    Adicionar arquivos de projeto.

---
+++ WeatherApp/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WeatherApp.Data.Entities;
+
+namespace WeatherApp.Data
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ WeatherApp/Data/SeedDb.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System;
+using WeatherApp.Data.Entities;
+using WeatherApp.Helpers;
+
+namespace WeatherApp.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+
+        public SeedDb(DataContext context, IUserHelper userHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+        }
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+           
+
+            //Add User Admin when create db
+            var userAdmin = await _userHelper.GetUserByEmailAsync("CatiaSantos@gmail.com");
+            if (userAdmin == null)
+            {
+                userAdmin = new User
+                {
+                    Email = "CatiaSantos@gmail.com",
+                    UserName = "CatiaSantos@gmail.com",
+                    Password = "Catia0202",
+                    FirstName = "Catia",
+                    LastName = "Santos"
+                };
+                var result1 = await _userHelper.AddUserAsync(userAdmin, "Catia0202");
+                if (result1 != IdentityResult.Success)
+                {
+                    throw new InvalidOperationException("Could not create the user  in seeder");
+                }
+               
+            }
+           
+        }
+    }
+}
